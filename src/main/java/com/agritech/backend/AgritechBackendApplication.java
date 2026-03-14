aaaeab258935ec1d661a1439aae096d101ef243f
dharabansal <dharabansal6@gmail.com>
2026-03-14 16:19:13 +0530

    Initial AgriTech Spring Boot backend setup

---
+++ src/main/java/com/agritech/backend/AgritechBackendApplication.java
@@ -0,0 +1,13 @@
+package com.agritech.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgritechBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgritechBackendApplication.class, args);
+    }
+
+}

859a356a062940580eda0a4c5b954d45f21ba3c7
dharabansal <dharabansal6@gmail.com>
2026-03-14 17:08:55 +0530

    IAdded Farmer entity using Lombok

---
+++ src/main/java/com/agritech/backend/model/Farmer.java
@@ -0,0 +1,27 @@
+package com.agritech.backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Farmer {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String village;
+    private String crop;
+
+
+
+}
+
